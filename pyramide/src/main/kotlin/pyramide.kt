e186080cd06695ee2385cdc8ad4509c22f79eb04
imane-aj <ajroudi.im@gmail.com>
2023-02-09 12:05:16 +0100

    Pyrimide

---
+++ pyramide/src/main/kotlin/pyramide.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val h = 0
+
+    for (x in 5 downTo h) {
+        for (j in 5 downTo h + x + 1) {
+            print(" ")
+        }
+        for (col in 0 until 2 * x + 1) {
+            print("*")
+        }
+        println()
+    }
+}
