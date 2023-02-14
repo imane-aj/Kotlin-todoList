50e3ffab033bb4bcd6d0b68d64d49c95b9b8d71c
imane-aj <ajroudi.im@gmail.com>
2023-02-14 10:08:42 +0100

    up

---
+++ autoFormation/Class_Object/src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+   val products = Produit()
+    while(true) {
+        println("Enter your choice: -1: to add new product -2: To see your products -3: To delete a product -4: to Exit")
+        val choice = readln()!!
+        when(choice) {
+            "1" -> {
+               println("Enter your product: ")
+                val product = readln()!!
+                products.add(product)
+            }
+            "2" -> {
+                products.get()
+            }
+            "3" -> {
+                println("witch product do you like to delete [${products.products}]")
+                val product = readln()!!
+                products.delete(product)
+            }
+            "4" -> break
+            else -> println("You're out")
+        }
+    }
+}
--- autoFormation/Class_Object/src/main/kotlin/Produit.kt
+++ autoFormation/Class_Object/src/main/kotlin/Produit.kt
@@ -1,2 +1,17 @@
 class Produit {
+    var products =  mutableListOf<String>("Infinix", "Iphone")
+
+    fun add(product:String){
+        products.add(product)
+        println("product added: $products")
+    }
+    fun get(){
+        println("Your products are: $products")
+    }
+
+    fun delete(product:String){
+        products.remove(product)
+        println("product deleted: $products")
+    }
+
 }
