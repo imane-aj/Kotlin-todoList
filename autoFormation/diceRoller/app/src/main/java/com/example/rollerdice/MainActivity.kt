e7df11d0cd94b196c7c2438ca582fb905a8e23c0
imane-aj <ajroudi.im@gmail.com>
2023-02-17 13:37:19 +0100

    image

--- autoFormation/diceRoller/app/src/main/java/com/example/rollerdice/MainActivity.kt
+++ autoFormation/diceRoller/app/src/main/java/com/example/rollerdice/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -19,10 +20,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         Toast.makeText(this,"Roller dice", Toast.LENGTH_SHORT).show()
-        val dice = Dice(100)
+        val dice = Dice(6)
         var diceRoller = dice.roll()
         val text = findViewById<TextView>(R.id.textView)
         text.setText(diceRoller.toString())
+
+        val diceImg = Dice(6)
+        val img = findViewById<ImageView>(R.id.imageView2)
+        when (diceRoller) {
+            1 -> img.setImageResource(R.drawable.dice_1)
+            2 -> img.setImageResource(R.drawable.dice_2)
+            3 -> img.setImageResource(R.drawable.dice_3)
+            4 -> img.setImageResource(R.drawable.dice_4)
+            5 -> img.setImageResource(R.drawable.dice_5)
+            6 -> img.setImageResource(R.drawable.dice_6)
+        }
+
     }
 
     class Dice(val numDice:Int){
