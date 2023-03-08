1a769c57c6194da9e419546b0cdd3c0b9727f582
imane-aj <ajroudi.im@gmail.com>
2023-03-08 11:51:01 +0100

    updates

--- prototypes/prototype1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ prototypes/prototype1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.LayoutInflater
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.Switch
 import android.widget.TextView
+import android.widget.Toast
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -43,12 +46,32 @@ class MainActivity : AppCompatActivity() {
   }
 
     fun update(position: Int) {
+        val data = task[position]
         val inflter = LayoutInflater.from(this)
         val v = inflter.inflate(R.layout.update_task, null)
-        val data = task[position]
-        //data.task = newTask
-        //data.hour = newHour
-        adapter.setItems(task)
+
+        var taskUp = v.findViewById<TextView>(R.id.editTask)
+        taskUp.setText(data.task)
+
+        val hourUp = v.findViewById<TextView>(R.id.edithour)
+
+        val addDialog = AlertDialog.Builder(this)
+        addDialog.setView(v)
+        addDialog.setPositiveButton("OK"){
+            dialog,_->
+            data.task = taskUp.text.toString()
+            data.hour = hourUp.text.toString().toInt()
+            adapter.setItems(task)
+            dialog.dismiss()
+        }
+        addDialog.setNegativeButton("Cancel"){
+            dialog,_->
+            dialog.dismiss()
+            Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show()
+        }
+        addDialog.create()
+        addDialog.show()
+
     }
 }
 
